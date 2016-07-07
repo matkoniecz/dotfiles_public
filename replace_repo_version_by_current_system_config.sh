0061102985938d9ec02d8785e8ef486d45114d02
Mateusz Konieczny <matkoniecz@gmail.com>
2016-07-07 15:25:40 +0200

    make possible to run it from outside of this folder

--- replace_repo_version_by_current_system_config.sh
+++ replace_repo_version_by_current_system_config.sh
@@ -1,2 +1,4 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+cd $DIR
 cp ~/.gitconfig . 
 cp ~/.gitignore_global . 
--- replace_system_settings_by_version_from_repo.sh
+++ replace_system_settings_by_version_from_repo.sh
@@ -1,2 +1,4 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+cd $DIR
 cp .gitconfig ~/.gitconfig
 cp .gitignore_global ~/.gitignore_global
