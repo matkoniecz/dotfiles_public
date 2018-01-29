6034c759f84f5f39a6fb7224a5ffa4ae9fb16f99
Mateusz Konieczny <matkoniecz@gmail.com>
2018-01-29 10:18:06 +0100

    import .bashrc from /etc/skel/, basic soft link creation

---
+++ create_soft_links.sh
@@ -0,0 +1,2 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+ln -s $DIR/.bashrc ~/.bashrc
