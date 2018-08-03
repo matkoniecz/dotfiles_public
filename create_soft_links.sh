7e5084903c7799c6c9236101794fc2cfdbe793c2
Mateusz Konieczny <matkoniecz@gmail.com>
2018-08-03 10:40:26 +0200

    remove autostart, rewrite soft lionk generations

--- create_soft_links.sh
+++ create_soft_links.sh
@@ -7,31 +7,24 @@ err_report() {
 trap 'err_report $LINENO' ERR
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #from http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
-if [ -e "$HOME/.bashrc" ]; then
-  rm "$HOME/.bashrc"
-fi
-if [ -e "$HOME/.config/lxsession/Lubuntu/desktop.conf" ]; then
-  rm "$HOME/.config/lxsession/Lubuntu/desktop.conf"
-fi
-if [ -e "$HOME/.config/lxsession/Lubuntu/autostart" ]; then
-  rm "$HOME/.config/lxsession/Lubuntu/autostart"
-fi
+
+prepare_for_linking() {
+  if [ -L "$1" ]; then
+    rm "$1"
+  fi
+  if [ -e "$1" ]; then
+    echo "file $1 exists and is not a symbolic link, was not removed to prevent accidental damage"
+  fi
+}
+
+prepare_for_linking "$HOME/.bashrc"
+prepare_for_linking "$HOME/.config/lxsession/Lubuntu/desktop.conf"
+prepare_for_linking "$HOME/.gitignore_global"
+prepare_for_linking "$HOME/.gitconfig"
+prepare_for_linking "$HOME/.gitattributes"
 
 ln -s "$DIR/.bashrc" "$HOME/.bashrc"
 ln -s "$DIR/desktop.conf" "$HOME/.config/lxsession/Lubuntu/desktop.conf"
-ln -s "$DIR/autostart" "$HOME/.config/lxsession/Lubuntu/autostart"
-
-if [ -e "$HOME/.gitignore_global" ]; then
-  rm "$HOME/.gitignore_global"
-  echo "$HOME/.gitignore_global"
-fi
-if [ -e "$HOME/.gitconfig" ]; then
-  rm "$HOME/.gitconfig"
-fi
-if [ -e "$HOME/.gitattributes" ]; then
-  rm "$HOME/.gitattributes"
-fi
-
 ln -s "$DIR/.gitignore_global" "$HOME/.gitignore_global"
 ln -s "$DIR/.gitconfig" "$HOME/.gitconfig"
 ln -s "$DIR/.gitattributes" "$HOME/.gitattributes"
