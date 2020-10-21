bdd5787e1a3b65244b5a67774de840bc1670d06d
Mateusz Konieczny <matkoniecz@gmail.com>
2020-10-21 19:01:14 +0200

    rudimentary windows support

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ g ll
 ```
 
 Why `exec bash`? See http://stackoverflow.com/questions/2518127/how-do-i-reload-bashrc-without-logging-out-and-back-in
+
+
+# Windows
+
+Open command line as administartor [because Windows is stupid](https://superuser.com/questions/129299/got-not-sufficient-privileges-message-in-cmd-when-logged-on-as-administrator)
+
+```
+cd %HOMEPATH%\Documents
+git clone https://github.com/matkoniecz/my_public_dotfiles.git
+cd my_public_dotfiles
+ruby install.rb
+```
--- install.rb
+++ install.rb
@@ -13,15 +13,46 @@ end
 
 def link(target, link_location)
     prepare_for_linking(link_location)
-    `ln -s "#{target}" "#{link_location}"`
+	if Gem.win_platform?
+		# Windows
+		puts	"mklink  \"#{link_location}\" \"#{target}\""
+		`mklink  "#{link_location}" "#{target}"`
+	else
+		# Hopefully actually Linux
+		`ln -s "#{target}" "#{link_location}"`
+	end
 end
 
-# link config files directly
-link("#{__dir__}/.bashrc", "$HOME/.bashrc")
-link("#{__dir__}/.gitignore_global", "$HOME/.gitignore_global")
-link("#{__dir__}/.gitconfig", "$HOME/.gitconfig")
-link("#{__dir__}/.gitattributes", "$HOME/.gitattributes")
+# https://stackoverflow.com/a/21468976
+if Gem.win_platform?
+	# Windows
+	
+	# and that is why I hate Windows...
+	# https://stackoverflow.com/questions/4050905/changing-gitconfig-location-on-windows
+	# https://superuser.com/questions/129299/got-not-sufficient-privileges-message-in-cmd-when-logged-on-as-administrator
 
-# setup links used by .bashrc
-link("#{__dir__}/my_addditions_to_bashrc.sh", "$HOME/.config/my_addditions_to_bashrc.sh")
-link("#{__dir__}/git_aliases.sh", "$HOME/.config/git_aliases.sh")
+	link_location = File.join("%HOMEPATH%", ".gitconfig")
+	target = File.join(__dir__, ".gitconfig")
+	link(target, link_location)
+	
+	link_location = File.join("%HOMEPATH%", ".gitattributes")
+	target = File.join(__dir__, ".gitattributes")
+	link(target, link_location)
+	
+	link_location = File.join("%HOMEPATH%", ".gitignore_global")
+	target = File.join(__dir__, ".gitignore_global")
+	link(target, link_location)
+	
+else
+	# Hopefully actually Linux
+
+	# link config files directly
+	link("#{__dir__}/.bashrc", "$HOME/.bashrc")
+	link("#{__dir__}/.gitignore_global", "$HOME/.gitignore_global")
+	link("#{__dir__}/.gitconfig", "$HOME/.gitconfig")
+	link("#{__dir__}/.gitattributes", "$HOME/.gitattributes")
+
+	# setup links used by .bashrc
+	link("#{__dir__}/my_addditions_to_bashrc.sh", "$HOME/.config/my_addditions_to_bashrc.sh")
+	link("#{__dir__}/git_aliases.sh", "$HOME/.config/git_aliases.sh")
+end
