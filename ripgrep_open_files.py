9bb2b7729688bfbabe0b14905ba01b03ebc3af07
Mateusz Konieczny <matkoniecz@gmail.com>
2022-10-04 21:24:00 +0200

    fix opening files with ripgrep

--- ripgrep_open_files.py
+++ ripgrep_open_files.py
@@ -3,6 +3,10 @@ import subprocess
 import os
 
 searched = sys.argv[1]
-returned = subprocess.check_output(["rg", "-l", searched]).decode('utf-8').strip().split("\n")
-for file in returned:
-    os.system('codium "' + file + '"')
+try:
+    returned = subprocess.check_output(["rg", "-l", searched]).decode('utf-8').strip().split("\n")
+    for file in returned:
+        os.system('codium "' + file + '"')
+except subprocess.CalledProcessError:
+    print("probably nothing found (subprocess.CalledProcessError)")
+    pass
